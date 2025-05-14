23195e9c64ad821c7fccf8424ae99be63d73b45f
Facundo Giannone <facuhgs@gmail.com>
2025-05-14 20:45:53 -0300

    :%

---
+++ PruebaGit.Entidades/Persona.cs
@@ -0,0 +1,12 @@
+﻿namespace PruebaGit.Entidades
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = null!;
+        public string Apellido { get; set; }=null!;
+
+
+
+    }
+}
